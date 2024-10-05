4e71e4e3b7720fb72faeac2dd950cd54a568a421
Khaji Mahammad Sami <111078844+SamiKhaji@users.noreply.github.com>
2024-10-06 02:09:20 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
-pip install langchain
 import streamlit as st
-from langchain_community.document_loaders import WebBaseLoader
+from langchain.document_loaders import WebBaseLoader  # Ensure this import is correct
 from chains import Chain
 from portfolio import Portfolio
 from utils import clean_text
+
 
 
 def create_streamlit_app(llm, portfolio, clean_text):
